4f144efe5fa1b2ec2403bad755d8df4bef5dc1af
Ryan Freeth <ryan@ryanfreeth.com>
2013-11-16 20:59:47 -0800

    Add express

--- LayoverCloudCode/cloud/main.js
+++ LayoverCloudCode/cloud/main.js
@@ -1,6 +1,18 @@
-
+require('cloud/app.js');
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
+});
+
+Parse.Cloud.define("users", function(request, response) {
+  var query = new Parse.Query("User");
+  query.find({
+    success: function(results) {
+      response.success(query);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
 });
